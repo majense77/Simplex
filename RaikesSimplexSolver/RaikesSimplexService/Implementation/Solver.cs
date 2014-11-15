f72b5a7af688d1d80208674d49fddf76ddbdb88a
Michael Jensen <majense77@gmail.com>
2014-11-15 14:23:50 -0600

    Objective Function prints now

--- RaikesSimplexSolver/RaikesSimplexService/Implementation/Solver.cs
+++ RaikesSimplexSolver/RaikesSimplexService/Implementation/Solver.cs
@@ -21,14 +21,34 @@ namespace RaikesSimplexService.Joel
             //Then compute slack, surplus and artificial vars
             //Print out standard form of matrix (think LPSolve)
 
+            PrintInput(model);
+
             foreach (LinearConstraint LC in model.Constraints) 
             {
                 for (int i = 0; i < LC.Coefficients.Length; i++) 
                 {
-                    System.Diagnostics.Debug.WriteLine(LC.Coefficients[i]);
+                    //System.Diagnostics.Debug.WriteLine(LC.Coefficients[i]);
                 }
             }
             return null;
+        }
+
+        public void PrintInput(Model model) 
+        {
+            System.Diagnostics.Debug.WriteLine("Original Data:");
+
+            //Objective Function
+            int goalCounter = 1;
+            System.Diagnostics.Debug.WriteLine("Objective Function:");
+            System.Diagnostics.Debug.Write(model.GoalKind + ": Z = ")
+            foreach (Double coeff in model.Goal.Coefficients) 
+            {
+                System.Diagnostics.Debug.Write(coeff + "x" + goalCounter + " + ");
+                goalCounter++;
+            }
+            System.Diagnostics.Debug.WriteLine(model.Goal.ConstantTerm);
+            
+
         }
     }
 }
