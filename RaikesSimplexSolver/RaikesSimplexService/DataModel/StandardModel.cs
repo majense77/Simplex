04012b30ddfdf3cc89fd708c1415b0ded768645c
rdahlman3 <redahl01pps@gmail.com>
2014-11-15 14:49:57 -0600

    Added class to hold artificial and surplus vars

---
+++ RaikesSimplexSolver/RaikesSimplexService/DataModel/StandardModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaikesSimplexService.DataModel
+{
+    class StandardModel : Model
+    {
+
+        public double[] sVariables { get; set; }
+
+        public double[] artificialVars { get; set; }
+    }
+}
